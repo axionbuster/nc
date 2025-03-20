62fa9d8022d779d3473c524cf4c0851362bf7358
axionbuster <axionbuster@gmail.com>
2025-03-20 00:07:40 -0700

    Show, Eq

--- src/Language/NC/Syn.hs
+++ src/Language/NC/Syn.hs
@@ -13,11 +13,19 @@ type Name a = Text
 -- | Placeholder for expression
 data Expr a = Expr a
 
+instance Eq (Expr a) where
+  _ == _ = True
+
+instance Show (Expr a) where
+  show _ = "Expr"
+
 -- | Fields for structs, unions, and union struct cases.
 data Field a
   = Field (FldX a) (Type a) (Name a)
   | -- | Bit fields are highly restricted.
     BitField (FldX a) (Type a) (Name a)
+
+deriving instance (PhaseEqShow a) => Show (Field a)
 
 -- Struct/Union/UnionStruct symbols live in a different namespace than
 -- terms, but Struct/Union/UnionStruct symbols share the same namespace.
@@ -47,12 +55,88 @@ data Type a
     Function (FunX a) (Maybe [Type a]) [Type a] (Type a)
   | Pointer (PtrX a) (QualifiedType a)
 
+deriving instance (PhaseEqShow a) => Show (Type a)
+
 -- | A case in a union struct. All cases must have a name.
 -- Case names live in the same namespace as tags do.
 data Case a = Case (CaseX a) (Name a) [Field a]
 
+deriving instance (PhaseEqShow a) => Show (Case a)
+
 -- | A declaration. I'm not including attributes, yet.
 data Dec a = Dec (DecX a) Linkage StorageClass (QualifiedType a) (Name a)
+
+deriving instance (PhaseEqShow a) => Show (Dec a)
+
+-- | Non-derived, primitive types
+data PrimType
+  = -- | non-character integral types
+    Int Signed IntLen
+  | -- | character types
+    Char (Maybe Signed)
+  | -- | real or complex floating point
+    Float FloatType
+  | -- | the @void@ type
+    Void
+  deriving (Eq, Show)
+
+data Completeness = Complete | Incomplete
+  deriving (Eq, Show)
+
+data Signed = Signed | Unsigned
+  deriving (Eq, Show)
+
+data IntLen = IntLen | Short | Long | LongLong
+  deriving (Eq, Show)
+
+data FloatType = Real RealFloatType | Complex RealFloatType
+  deriving (Eq, Show)
+
+data RealFloatType = RFFloat | RFDouble | RFLongDouble
+  deriving (Eq, Show)
+
+data QualifiedType a = Qualified (QualX a) Qualifier (Type a)
+
+deriving instance (PhaseEqShow x) => Show (QualifiedType x)
+
+data Qualifier = Qualifier
+  {const :: Bool, volatile :: Bool, restrict :: Bool}
+  deriving (Eq, Show)
+
+-- | Storage class and linkage specifiers
+data StorageClass = Static | Extern | Register | Auto | ThreadLocal | NoStorage
+  deriving (Eq, Show)
+
+-- | Separate linkage type if needed
+data Linkage = Internal | External | NoLinkage
+  deriving (Eq, Show)
+
+-- | Constraints for all phase annotations, requiring Eq and Show
+class
+  ( Eq (PrimX a),
+    Show (PrimX a),
+    Eq (ArrX a),
+    Show (ArrX a),
+    Eq (StrX a),
+    Show (StrX a),
+    Eq (UniX a),
+    Show (UniX a),
+    Eq (UnsX a),
+    Show (UnsX a),
+    Eq (FunX a),
+    Show (FunX a),
+    Eq (PtrX a),
+    Show (PtrX a),
+    Eq (DecX a),
+    Show (DecX a),
+    Eq (CaseX a),
+    Show (CaseX a),
+    Eq (FldX a),
+    Show (FldX a),
+    Eq (QualX a),
+    Show (QualX a)
+  ) =>
+  PhaseEqShow a
 
 type family PrimX a
 
@@ -94,41 +178,9 @@ type instance CaseX () = ()
 
 type instance FldX () = ()
 
--- | Non-derived, primitive types
-data PrimType
-  = -- | non-character integral types
-    Int Signed IntLen
-  | -- | character types
-    Char (Maybe Signed)
-  | -- | real or complex floating point
-    Float FloatType
-  | -- | the @void@ type
-    Void
-
-data Completeness = Complete | Incomplete
-
-data Signed = Signed | Unsigned
-
-data IntLen = IntLen | Short | Long | LongLong
-
-data FloatType = Real RealFloatType | Complex RealFloatType
-
-data RealFloatType = RFFloat | RFDouble | RFLongDouble
-
-data QualifiedType a = Qualified (QualX a) Qualifier (Type a)
-
-data Qualifier = Qualifier
-  {const :: Bool, volatile :: Bool, restrict :: Bool}
-
 qualzero :: Qualifier
 qualzero = Qualifier False False False
 
 type family QualX a
 
 type instance QualX () = ()
-
--- | Storage class and linkage specifiers
-data StorageClass = Static | Extern | Register | Auto | ThreadLocal | NoStorage
-
--- | Separate linkage type if needed
-data Linkage = Internal | External | NoLinkage
