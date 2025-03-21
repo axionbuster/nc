86b207632b703ae342386a149b138a33702bd737
axionbuster <axionbuster@gmail.com>
2025-03-21 12:12:23 -0700

    must-parse cases. oops, long double _Complex FAIL
    checkpt OK/Right/NEQ
    
    need to inspect now

--- test/Main.hs
+++ test/Main.hs
@@ -19,7 +19,40 @@ data PT = PT String (Either Error CT.PrimType)
 ptlist1 :: [PT]
 ptlist1 =
   [ PT "int" (Right CT.Int_),
-    PT "char" (Right CT.Char_)
+    PT "char" (Right CT.Char_),
+    PT "void" (Right CT.Void),
+    PT "signed char" (Right (CT.Char (Just CT.Signed))),
+    PT "unsigned char" (Right (CT.Char (Just CT.Unsigned))),
+    PT "short" (Right CT.Short_),
+    PT "signed short" (Right CT.Short_),
+    PT "short int" (Right CT.Short_),
+    PT "signed short int" (Right CT.Short_),
+    PT "unsigned short" (Right CT.UShort_),
+    PT "unsigned short int" (Right CT.UShort_),
+    PT "int" (Right CT.Int_),
+    PT "signed" (Right CT.Int_),
+    PT "signed int" (Right CT.Int_),
+    PT "unsigned" (Right CT.UInt_),
+    PT "unsigned int" (Right CT.UInt_),
+    PT "long" (Right CT.Long_),
+    PT "signed long" (Right CT.Long_),
+    PT "long int" (Right CT.Long_),
+    PT "signed long int" (Right CT.Long_),
+    PT "unsigned long" (Right CT.ULong_),
+    PT "unsigned long int" (Right CT.ULong_),
+    PT "long long" (Right CT.LongLong_),
+    PT "signed long long" (Right CT.LongLong_),
+    PT "long long int" (Right CT.LongLong_),
+    PT "signed long long int" (Right CT.LongLong_),
+    PT "unsigned long long" (Right CT.ULongLong_),
+    PT "unsigned long long int" (Right CT.ULongLong_),
+    PT "float" (Right CT.Float_),
+    PT "double" (Right CT.Double_),
+    PT "long double" (Right CT.LongDouble_),
+    PT "_Bool" (Right CT.Bool),
+    PT "float _Complex" (Right CT.ComplexFloat_),
+    PT "double _Complex" (Right CT.ComplexDouble_),
+    PT "long double _Complex" (Right CT.ComplexLongDouble_)
   ]
 
 runpttests :: [PT] -> [TestTree]
