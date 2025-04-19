7b5a94e2cddd5e9402c71514f9303b2ba95f91ed
YuJin Kim <axionbuster@gmail.com>
2025-04-18 20:24:16 -0700

    parse tokens for declspec
    reason: type specifiers and qualifiers and alignment specifiers, and other declaration specifiers, share many keywords. it makes sense to use a big optimized radix tree where all keywords are matched all at once. so, abstract out the part that reacts to tokens.

--- src/NC/Parser/Decl.hs
+++ src/NC/Parser/Decl.hs
@@ -85,126 +85,168 @@ __bool mask = lens g s
   g = (== mask) . (.&. mask)
   s x = \case True -> x .|. mask; _ -> x .&. complement mask
 
-sqa_const, sqa_restrict, sqa_volatile, sqa_atomic :: Lens' SQA Bool
-sqa_const = __bool 0x01
-sqa_restrict = __bool 0x02
-sqa_volatile = __bool 0x04
-sqa_atomic = __bool 0x08
+-- Pattern synonyms for SQA flags
+{-# COMPLETE
+  SqaConst,
+  SqaRestrict,
+  SqaVolatile,
+  SqaAtomic,
+  SqaExtern,
+  SqaAuto,
+  SqaStatic,
+  SqaTypedef,
+  SqaThreadLocal,
+  SqaConstexpr,
+  SqaRegister,
+  SqaInt,
+  SqaBool,
+  SqaChar,
+  SqaShort,
+  SqaLong,
+  SqaLongLong,
+  SqaBitInt,
+  SqaFloat,
+  SqaDouble,
+  SqaComplex,
+  SqaD32,
+  SqaD64,
+  SqaD128,
+  SqaVoid,
+  SqaNullptr,
+  SqaStruct,
+  SqaUnion,
+  SqaEnum,
+  SqaSigned,
+  SqaUnsigned,
+  SqaInline,
+  SqaNoreturn,
+  SqaAlignas,
+  SqaImaginary,
+  SqaTypeof,
+  SqaTypeofUnqual,
+  SqaAtomicnewtype,
+  SqaTypedefname
+  #-}
 
-sqa_extern, sqa_auto, sqa_static, sqa_typedef :: Lens' SQA Bool
-sqa_extern = __bool 0x10
-sqa_auto = __bool 0x20
-sqa_static = __bool 0x40
-sqa_typedef = __bool 0x80
+pattern SqaConst, SqaRestrict, SqaVolatile, SqaAtomic :: SQA
+pattern SqaConst = 1
+pattern SqaRestrict = 2
+pattern SqaVolatile = 4
+pattern SqaAtomic = 8
 
-sqa_thread_local, sqa_constexpr, sqa_register, sqa_int :: Lens' SQA Bool
-sqa_thread_local = __bool 0x100
-sqa_constexpr = __bool 0x200
-sqa_register = __bool 0x400
-sqa_int = __bool 0x800
+pattern SqaExtern, SqaAuto, SqaStatic, SqaTypedef :: SQA
+pattern SqaExtern = 0x10
+pattern SqaAuto = 0x20
+pattern SqaStatic = 0x40
+pattern SqaTypedef = 0x80
 
-sqa_bool, sqa_char, sqa_short, sqa_long :: Lens' SQA Bool
-sqa_bool = __bool 0x1000
-sqa_char = __bool 0x2000
-sqa_short = __bool 0x4000
-sqa_long = __bool 0x8000
+pattern SqaThreadLocal, SqaConstexpr, SqaRegister, SqaInt :: SQA
+pattern SqaThreadLocal = 0x100
+pattern SqaConstexpr = 0x200
+pattern SqaRegister = 0x400
+pattern SqaInt = 0x800
 
-sqa_longlong, sqa_bitint, sqa_float, sqa_double :: Lens' SQA Bool
+pattern SqaBool, SqaChar, SqaShort, SqaLong :: SQA
+pattern SqaBool = 0x1000
+pattern SqaChar = 0x2000
+pattern SqaShort = 0x4000
+pattern SqaLong = 0x8000
 
--- | @long@ and @long long@ are treated specially. It's permissible
+pattern SqaLongLong, SqaBitInt, SqaFloat, SqaDouble :: SQA
+-- @long@ and @long long@ are treated specially. It's permissible
 -- for @long@ to occur at most twice. So, if @long long@ is seen, I
--- want @long@ to test positive. Thus, I made this to set sqa_long as well.
-sqa_longlong = __bool 0x1_8000
+-- want @long@ to test positive. Thus, I made this to set SqaLong as well.
+pattern SqaLongLong = 0x1_8000
+pattern SqaBitInt = 0x2_0000
+pattern SqaFloat = 0x4_0000
+pattern SqaDouble = 0x8_0000
 
-sqa_bitint = __bool 0x2_0000
+pattern SqaComplex, SqaD32, SqaD64, SqaD128 :: SQA
+pattern SqaComplex = 0x10_0000
+pattern SqaD32 = 0x20_0000
+pattern SqaD64 = 0x40_0000
+pattern SqaD128 = 0x80_0000
 
-sqa_float = __bool 0x4_0000
+pattern SqaVoid, SqaNullptr, SqaStruct, SqaUnion :: SQA
+pattern SqaVoid = 0x100_0000
+pattern SqaNullptr = 0x200_0000
+pattern SqaStruct = 0x400_0000
+pattern SqaUnion = 0x800_0000
 
-sqa_double = __bool 0x8_0000
+pattern SqaEnum, SqaSigned, SqaUnsigned, SqaInline :: SQA
+pattern SqaEnum = 0x1000_0000
+pattern SqaSigned = 0x2000_0000
+pattern SqaUnsigned = 0x4000_0000
+pattern SqaInline = 0x8000_0000
 
-sqa_complex, sqa_d32, sqa_d64, sqa_d128 :: Lens' SQA Bool
-sqa_complex = __bool 0x10_0000
-sqa_d32 = __bool 0x20_0000
-sqa_d64 = __bool 0x40_0000
-sqa_d128 = __bool 0x80_0000
+pattern SqaNoreturn, SqaAlignas, SqaImaginary, SqaTypeof :: SQA
+pattern SqaNoreturn = 0x1_0000_0000
+pattern SqaAlignas = 0x2_0000_0000
+pattern SqaImaginary = 0x4_0000_0000
+pattern SqaTypeof = 0x8_0000_0000
 
-sqa_void, sqa_nullptr, sqa_struct, sqa_union :: Lens' SQA Bool
-sqa_void = __bool 0x100_0000
-sqa_nullptr = __bool 0x200_0000
-sqa_struct = __bool 0x400_0000
-sqa_union = __bool 0x800_0000
+pattern SqaTypeofUnqual, SqaAtomicnewtype, SqaTypedefname :: SQA
+pattern SqaTypeofUnqual = 0x10_0000_0000
+pattern SqaAtomicnewtype = 0x20_0000_0000
+pattern SqaTypedefname = 0x40_0000_0000
 
-sqa_enum, sqa_signed, sqa_unsigned, sqa_inline :: Lens' SQA Bool
-sqa_enum = __bool 0x1000_0000
-sqa_signed = __bool 0x2000_0000
-sqa_unsigned = __bool 0x4000_0000
-sqa_inline = __bool 0x8000_0000
-
-sqa_noreturn, sqa_alignas, sqa_imaginary, sqa_typeof :: Lens' SQA Bool
-sqa_noreturn = __bool 0x1_0000_0000
-sqa_alignas = __bool 0x2_0000_0000
-sqa_imaginary = __bool 0x4_0000_0000
-sqa_typeof = __bool 0x8_0000_0000
-
-sqa_typeof_unqual, sqa_atomicnewtype, sqa_typedefname :: Lens' SQA Bool
-sqa_typeof_unqual = __bool 0x10_0000_0000
-sqa_atomicnewtype = __bool 0x20_0000_0000
-sqa_typedefname = __bool 0x40_0000_0000
+-- | This will never match.
+pattern SqaOther :: SQA
+pattern SqaOther = 0xffff_ffff_ffff_ffff
 
 -- | Collect \'specifier-qualifier-list\' into a 'Type'. This
 -- involves applying 'specqualalign' many times.
 specqualaligns :: P Type
 specqualaligns = undefined
 
--- | Parse a single type-specifier, type-qualifier, or alignment-specifier.
--- (All these rules are merged to share bulk reading).
-specqualalign :: P TT
-specqualalign =
-  $( switch_ws1
-       [|
-         case _ of
-           "void" -> push sqa_void
-           "char" -> push sqa_char
-           "short" -> push sqa_short
-           "int" -> push sqa_int
-           "long" -> push sqa_long
-           "float" -> push sqa_float
-           "double" -> push sqa_double
-           "signed" -> push sqa_signed
-           "unsigned" -> push sqa_unsigned
-           "_BitInt" -> bitint
-           "_Bool" -> push sqa_bool
-           "_Complex" -> push sqa_complex
-           "_Decimal32" -> push sqa_d32
-           "_Decimal64" -> push sqa_d64
-           "_Decimal128" -> push sqa_d128
-           "_Atomic" -> atomic
-           "struct" -> struct
-           "union" -> union
-           "enum" -> enum
-           "typeof" -> typeof
-           "typeof_unqual" -> typeof_unqual
-           "alignas" -> alignas
-           "_Alignas" -> alignas
-           "const" -> push sqa_const
-           "restrict" -> push sqa_restrict
-           "volatile" -> push sqa_volatile
-           _ -> typedefname
-         |]
-   )
+-- | We use a case-of match instead of a direct string match because
+-- this portion is shared between different parsers. We want an efficient
+-- trie search which favors the case where every possibility is combined
+-- and tested at once.
+specqualalign_real :: SQA -> P TT
+specqualalign_real = \case
+  SqaVoid -> push SqaVoid
+  SqaChar -> push SqaChar
+  SqaShort -> push SqaShort
+  SqaInt -> push SqaInt
+  SqaLong -> push SqaLong
+  SqaFloat -> push SqaFloat
+  SqaDouble -> push SqaDouble
+  SqaSigned -> push SqaSigned
+  SqaUnsigned -> push SqaUnsigned
+  SqaBitInt -> bitint
+  SqaBool -> push SqaBool
+  SqaComplex -> push SqaComplex
+  SqaD32 -> push SqaD32
+  SqaD64 -> push SqaD64
+  SqaD128 -> push SqaD128
+  SqaAtomic -> atomic
+  SqaStruct -> struct
+  SqaUnion -> union
+  SqaEnum -> enum
+  SqaTypeof -> typeof
+  SqaTypeofUnqual -> typeof_unqual
+  SqaAlignas -> alignas
+  SqaConst -> push SqaConst
+  SqaRestrict -> push SqaRestrict
+  SqaVolatile -> push SqaVolatile
+  _nomatch -> do
+    traceIO "handling typedefname"
+    typedefname
  where
-  push (tok :: Lens' SQA Bool) = pure $ TT \tokens ->
-    -- test: been set already? generally duplicate tokens are bad.
-    if tokens ^. t_sqa . tok
-      then -- duplicate, check for one exception (long long).
-        if ((mempty & tok .~ True) == (mempty & sqa_long .~ True))
-          && not (tokens ^. t_sqa . sqa_longlong)
-          then -- oh, this is an exception.
-            tokens & t_sqa . sqa_longlong .~ True
-          else -- most likely case. log error.
-            tokens & t_dups . tok .~ True
-      else -- no problem, not been set before.
-        tokens & t_sqa . tok .~ True
+  s %&~ b = s %~ (.|. b)
+  push sqa = pure $ coerce \rt -> do
+    let rts = rt ^. t_sqa
+    if sqa .&. rts == sqa
+      then -- duplicate, unless long and no long long
+        if sqa == SqaLong && sqa .&. SqaLongLong == SqaLongLong
+          then -- oh, it's fine
+            rt & t_sqa %&~ sqa
+          else -- oops, bad
+            rt & t_dups %&~ sqa
+      else
+        -- admit token
+        rt & t_sqa %&~ sqa
   bitint = do
     -- FIXME: we need to admit a constant-expression, but we require
     -- a _BitInt type to only use an actual 16-bit integer. I think
@@ -234,31 +276,68 @@ specqualalign =
       tn <- typename `pcut_expect` "type name"
       pure
         $ TT (t_extra .~ Just (ExAtomic tn))
-        <> TT (t_sqa . sqa_atomicnewtype .~ True)
-    atoqua = push sqa_atomic
-  struct = (<>) <$> push sqa_struct <*> parserecord RecStruct
-  union = (<>) <$> push sqa_union <*> parserecord RecUnion
-  enum = (<>) <$> push sqa_enum <*> parseenum
+        <> TT (t_sqa %&~ SqaAtomicnewtype)
+    atoqua = push SqaAtomic
+  struct = (<>) <$> push SqaStruct <*> parserecord RecStruct
+  union = (<>) <$> push SqaUnion <*> parserecord RecUnion
+  enum = (<>) <$> push SqaEnum <*> parseenum
   typeof = typeof2 TQQual
   typeof_unqual = typeof2 TQUnqual
   alignas =
-    (<>) <$> push sqa_alignas <*> do
+    (<>) <$> push SqaAlignas <*> do
       cutlpar >> do
         t <-
           ((AlignAsType <$> typename) <|> (AlignAs <$> constexpr))
             `pcut_expect` "a type name or a constant expression"
         pure (TT (t_align .~ Just t)) <* cutrpar
-  typedefname = (<>) <$> push sqa_typedefname <*> undefined identifier
+  typedefname = (<>) <$> push SqaTypedefname <*> undefined identifier
   typeof2 qualification =
-    let lens2use :: Lens' SQA Bool
-        lens2use
-          | TQQual <- qualification = sqa_typeof
-          | otherwise = sqa_typeof_unqual
+    let field2use
+          | TQQual <- qualification = SqaTypeof
+          | otherwise = SqaTypeofUnqual
      in (<>)
-          <$> push lens2use
+          <$> push field2use
           <*> do
             t <- (TypeofType <$> typename) <|> (TypeofExpr <$> expr)
             pure $ TT (t_extra .~ Just (ExTypeof t qualification))
+
+-- | Parse a single type-specifier, type-qualifier, or alignment-specifier.
+-- (All these rules are merged to share bulk reading).
+specqualalign :: P TT
+specqualalign =
+  $( switch_ws1
+       [|
+         case _ of
+           "void" -> r SqaVoid
+           "char" -> r SqaChar
+           "short" -> r SqaShort
+           "int" -> r SqaInt
+           "long" -> r SqaLong
+           "float" -> r SqaFloat
+           "double" -> r SqaDouble
+           "signed" -> r SqaSigned
+           "unsigned" -> r SqaUnsigned
+           "_BitInt" -> r SqaBitInt
+           "_Bool" -> r SqaBool
+           "_Complex" -> r SqaComplex
+           "_Decimal32" -> r SqaD32
+           "_Decimal64" -> r SqaD64
+           "_Decimal128" -> r SqaD128
+           "_Atomic" -> r SqaAtomic
+           "struct" -> r SqaStruct
+           "union" -> r SqaUnion
+           "enum" -> r SqaEnum
+           "typeof" -> r SqaTypeof
+           "typeof_unqual" -> r SqaTypeofUnqual
+           "_Alignas" -> r SqaAlignas
+           "const" -> r SqaConst
+           "restrict" -> r SqaRestrict
+           "volatile" -> r SqaVolatile
+           _ -> r SqaOther
+         |]
+   )
+ where
+  r = specqualalign_real
 
 -- | Parse a record type and then return a type transformer (TT).
 parserecord :: RecType -> P TT
